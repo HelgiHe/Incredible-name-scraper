604a8b27398b118d81d0c1f2eed92b7b230b5bf8
Helgi Helgason <HelgiHel@gmail.com>
2018-09-10 14:45:51 +0000

    write to db

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
 const path = require('path');
 const fs = require('fs');
+const axios = require('axios');
 const cheerio = require('cheerio');
 const request = require('request');
-const names = require('./nameData');
-const URls = require('./urls');
+const admin = require('firebase-admin');
+const _ = require('lodash');
+const serviceAccount = require('./nofn-4cfa4-firebase-adminsdk-rtj3v-fa340427ed.json');
 
-// for (let name of names) {
-//   console.log(name.Nafn);
-// }
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://nofn-4cfa4.firebaseio.com'
+});
 
-request(
-  'https://attavitinn.is/sambond-og-kynlif/fjolskylda/merkingar-nafna/',
-  function(error, response, html) {
-    if (error) {
-      console.log(error);
+const nameCollection = [];
+axios
+  .get('http://attavitinn.is/sambond-og-kynlif/fjolskylda/merkingar-nafna/')
+  .then(response => {
+    groupData(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+function groupData(html) {
+  const $ = cheerio.load(html);
+  $('li.men').each(function (i, element) {
+    const nameDefArr = $(this)
+      .text()
+      .split(/[\r\n]+/);
+    const removeEmpty = nameDefArr.filter(name => name !== '');
+
+    if (typeof removeEmpty[1] !== 'undefined') {
+      nameCollection.push({ name: removeEmpty[0], desc: removeEmpty[1] });
     }
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
-      $('li.woman').each(function(i, element) {
-        var a = $(this).html();
-        console.log(
-          $(this)
-            .text()
-            .split(/[\r\n]+/),
-        );
-        // console.log($(this).html());
-      });
-    }
-  },
-);
+  });
+  const groupedNames = _.groupBy(nameCollection, item => item.name.charAt(0));
+  const alphabet = Object.keys(groupedNames);
+  // console.log(groupedNames);
+  alphabet.forEach(letter => {
+    writeToDb(letter, groupedNames[letter]);
+  });
+}
+
+function writeToDb(doc, data) {
+  const db = admin.firestore();
+  const setDoc = db
+    .collection('maleNames')
+    .doc(doc)
+    .set({ names: data });
+}
